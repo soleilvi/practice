342a18889fffa69925d291706dde7d0605e6d89c
soleilvi <soleil.vivero@gmail.com>
2023-08-19 16:17:03 -0700

    PalindromeNumber.cpp 2

--- LeetCode/Top Interview Questions/PalindromeNumber.cpp
+++ LeetCode/Top Interview Questions/PalindromeNumber.cpp
@@ -3,7 +3,7 @@ https://leetcode.com/problems/palindrome-number/
 
 By Soleil Vivero
 Conversion algorithm from https://math.stackexchange.com/questions/480068/how-to-reverse-digits-of-an-integer-mathematically
-08/18/23
+08/19/23
 */
 
 #include <cmath>
@@ -13,17 +13,23 @@ class Solution {
 public:
     bool isPalindrome(int x) 
     {
+        // A number with a negative sign will never be a palindrome
         if (x < 0)
         {
             return false;
         }
+        // A single digit will always be a palindrome
+        else if (x < 10)
+        {
+            return true;
+        }
 
-        float logx = std::round(std::log10(x));
+        double logx = std::round(std::log10(x));
         std::cout << "logx: " << logx << std::endl;
-        float factorsSum = 0;
-        float factor1 = 0;
-        float factor2 = 0;
-        float result = 0;
+        double factorsSum = 0;
+        double factor1 = 0;
+        double factor2 = 0;
+        double result = 0;
 
         for (int i = 1; i <= logx; i++)
         {
@@ -37,7 +43,8 @@ public:
             std::cout << std::endl;
         }
 
-        result = std::round(x * std::pow(10, logx) - 99 * factorsSum);  // calculation reverses order of digits in x
+        result = x * std::pow(10, logx) - 99 * factorsSum;  // calculation reverses order of digits in x
+        result = std::abs(std::round(result));;  // For the sake of clarity, rounding and converting to absolute value are on a different line
         std::cout << result << std::endl;
 
         if (static_cast<int>(result) == x)  // turn result into an integer to compare
@@ -50,3 +57,18 @@ public:
         }
     }
 };
+
+int main()
+{
+    int x = 88888;
+    Solution solution;
+
+    if (solution.isPalindrome(x))
+    {
+        std::cout << x << " is a palindrome" << std::endl;
+    }
+    else
+    {
+        std::cout << x << " is not a palindrome" << std::endl;
+    }
+}
