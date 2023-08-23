6f488e9f2a5e72e32da8445caa4458588298148c
soleilvi <soleil.vivero@gmail.com>
2023-08-23 11:54:38 -0700

    Longest Substring 2

--- LeetCode/Top Interview Questions/3LongestSubstring.cpp
+++ LeetCode/Top Interview Questions/3LongestSubstring.cpp
@@ -2,10 +2,11 @@
 https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
 By Soleil Vivero
-08/22/23
+08/23/23
 */
 
 #include <string>
+#include <iostream>
 #include <unordered_set>
 
 using namespace std;
@@ -13,16 +14,31 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        /*
-        1. Parse through string
-        2. If there are any, look through characters in set
-        3. If the letter being parsed is not in the set, add it to the set
-        4. If it is...
-            a. Record how many characters are in the set
-                - UNLESS a previously recorder int is greater!
-            b. Empty set
-            c. Add character to set
-        */
-        std::unordered_set<char> mySet;
+        std::unordered_set<char> charSet;
+        int longestSubstringLength = 0;
+
+        // Iterate through string characters
+        for (auto &ch : s)
+        {
+            if (charSet.contains(ch))
+            {
+                charSet.clear();
+            }
+
+            charSet.insert(ch);
+            if (charSet.size() > longestSubstringLength)
+            {
+                longestSubstringLength = charSet.size();
+            }
+        }
+        return longestSubstringLength;
     }
 };
+
+int main()
+{
+    Solution s;
+    std::string str = "Looop";
+
+    std::cout << s.lengthOfLongestSubstring(str);
+}
