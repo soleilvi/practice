dbee3da93490ad346f800b92ca928de07ce6d514
soleilvi <soleil.vivero@gmail.com>
2023-08-26 23:26:52 -0700

    Longest Substring Final

--- LeetCode/Top Interview Questions/3LongestSubstring.cpp
+++ LeetCode/Top Interview Questions/3LongestSubstring.cpp
@@ -2,18 +2,8 @@
 https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
 By Soleil Vivero
-08/25/23
-
-Solutions:
-    1. Have another for-loop inside of the character for-loop that goes through past sections of the string..? (O(n^2))
-    2. Don't clear everything in the map, just those characters that came before the char with a duplicate (O(n))
+08/26/23
 */
-
-#include <string>
-#include <iostream>
-#include <unordered_map>
-
-using namespace std;
 
 #include <unordered_map>
 
@@ -59,11 +49,3 @@ public:
         return longestSubstringLength;
     }
 };
-
-int main()
-{
-    Solution s;
-    std::string str = "cornycab";
-
-    std::cout << s.lengthOfLongestSubstring(str) << " <-- Result" << std::endl;
-}
