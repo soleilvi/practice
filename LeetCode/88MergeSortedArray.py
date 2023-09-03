6d1edbe12f3f2984295de97d89c167821aa75295
soleilvi <soleil.vivero@gmail.com>
2023-09-02 22:29:42 -0700

    Merge Sorted Array 1

--- LeetCode/88MergeSortedArray.py
+++ LeetCode/88MergeSortedArray.py
@@ -6,23 +6,17 @@ By Soleil Vivero
 09/02/23
 '''
 
-nums1 = [0, 2, 5, 0, 8]
-nums2 = [9, 5, 7, 8, 1]
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
 
-def remove_zeroes(list):
-    while 0 in list:
-        list.remove(0)
+        while 0 in nums1:
+            nums1.remove(0)
+        
+        while 0 in nums2:
+            nums2.remove(0)
 
-def merge(list1, list2):
-    list1 = sorted(list1 + list2)
-
-def transform(list):
-    list = [0, 9, 3, 4]
-
-print(nums1)
-remove_zeroes(nums1)
-print(nums1)
-merge(nums1, nums2)
-print(nums1)
-transform(nums1)
-print(nums1)
+        nums1 = sorted(nums1 + nums2)
+        print(nums1)
