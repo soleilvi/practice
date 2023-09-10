f125f9c637ec26e9871b1dcebe75952caa24ea6e
soleilvi <soleil.vivero@gmail.com>
2023-09-09 21:32:06 -0700

    Group Anagram 2

--- LeetCode/49GroupAnagrams.py
+++ LeetCode/49GroupAnagrams.py
@@ -3,7 +3,7 @@ Python 3
 https://leetcode.com/problems/group-anagrams/
 
 By Soleil Vivero
-09/08/23
+09/09/23
 '''
 
 class Solution:
@@ -17,7 +17,7 @@ class Solution:
 
             for sChar, tChar in zip(s, t):
                 count[sChar] += 1
-                tDict[tChar] -= 1
+                count[tChar] -= 1
             
             for val in count.values():
                 if val != 0:
@@ -28,11 +28,15 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groupedAnagrams = []
 
-        for i in range(len(strs)):
+        while strs:
             cols = []
-            cols.append(strs[i])
-            for j in range(len(strs) + 1):  # Out of range
-                if isAnagram(strs[i], strs[j]):
-                    cols.append(strs[j])
-                    # remove element
-            # remove element
+            cols.append(strs.pop())
+
+            for str in strs:
+                if self.isAnagram(str, cols[0]):
+                    cols.append(str)
+                    # strs.remove(str)
+            
+            groupedAnagrams.append(cols)
+
+        return groupedAnagrams
