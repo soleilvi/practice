21dad5972d9722c97ac7cff6a9fb20f407d15632
soleilvi <soleil.vivero@gmail.com>
2023-11-14 08:47:09 -0800

    Longest Consecutive Sequence 2

--- LeetCode/128LongestConsecutiveSequence.py
+++ LeetCode/128LongestConsecutiveSequence.py
@@ -1,19 +1,18 @@
-'''
-Fails when:
-- There is only one number in the list
-- There are only a couple numbers in the list
-'''
-
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         s = set(nums)
-        count = 0
+        l = [(0, 0)]  # Place edge numbers in tuples, then return the greatest difference between the two numbers
+        i = 0
 
         if len(s) == 1:
             return 1
 
         for num in s:
-            if num + 1 in s or num - 1 in s:
-                count += 1
+            if num + 1 in s and not num - 1 in s:
+                l[i][0] = num
+                i += 1
+            elif num - 2 in s and not num + 1 in s:
+                l[i][1] = num
+                i += 1
+                
         
-        return count
